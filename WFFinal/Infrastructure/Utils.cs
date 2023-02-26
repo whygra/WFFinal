7e185f058be4f16c9aa2fca9db3a1278fd9d9bec
Yaroslav Grabovsky <why.grabovsky@gmail.com>
2023-02-26 15:57:06 +0300

    Добавьте файлы проекта.

---
+++ WFFinal/Infrastructure/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFFinal.Models;
+
+namespace WFFinal.Infrastructure
+{
+    public static class Utils
+    {
+        public static string DataPath = "App_Data/";
+        public static string TempFilePath = DataPath + "routes.json";
+
+        // генерация случайных чисел
+        static Random Random = new Random();
+        public static int GetRand(int lo, int hi)
+        {
+            return Random.Next(lo, hi);
+        }
+        public static bool GetRand()
+        {
+            return Random.Next(2) == 0;
+        }
+
+        // вернуть случайный элемент коллекции
+        public static T SelectRand<T>(IEnumerable<T> collection)
+        {
+            int numElements = collection.Count();
+
+            return collection.ElementAt(GetRand(0, numElements));
+        }
+
+    }
+}
